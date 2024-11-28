7be2a0db5728b8ca0b613b26106471d86e2518a7
Sandra Misigo <misigosandra@gmail.com>
2024-11-28 11:30:11 +0300

    myphase1functionscontinuedjav

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+console.log(saturdayFun())
+  
+function mondayWork(task = "go to the office") {
+    return `This Monday, I will ${task}.`;
+  }
+  console.log(mondayWork())
+
+  function wrapAdjective(flair = "*") {
+    return function describe(adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  
+
